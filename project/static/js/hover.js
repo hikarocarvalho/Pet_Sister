f215c18a943b836a34e34a90f3ea8b90fd771f47
hikarocarvalho <hikarofcarvalho@gmail.com>
2021-08-08 13:05:55 -0300

    make comments for hmtl's and javascript files

--- project/static/js/hover.js
+++ project/static/js/hover.js
@@ -1,13 +1,19 @@
+// here we setup the image for hover in menu
+// aqui nos setamos a imagem para a transição no menu
 function hoverFuncion(value){
     var iditem = "img" + value;
     document.getElementById(iditem).style.backgroundImage = 'url("static/img/dog-hands.png")';
     document.getElementById(iditem).style.backgroundSize = 'auto 100%';
     document.getElementById(iditem).style.backgroundRepeat = 'no-repeat';
 }
+// here is the function to reset state of item Image
+// aqui esta a função para resetar a imgem do botão
 function liaveFunction(value){
     var iditem = "img" + value;
     document.getElementById(iditem).style.backgroundImage = 'none';
 }
+// here is the function to set the image in a selected item
+// aqui está a funçãp que seta a imagem para o item selecionado
 function clickFunction(value){
     document.getElementById("internalPage").src=value;
 }
--- project/static/js/redirect.js
+++ project/static/js/redirect.js
@@ -1,3 +1,5 @@
+// here is the funcion to make the redirect when the user make a login
+// aqui está a função para fazer o redirecionamento quando o usuário faz login
 function load(){
     setTimeout(function() {
         window.location.href = "/profile";}
